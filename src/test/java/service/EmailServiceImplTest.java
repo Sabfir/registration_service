babe295a92d921b79ca7fc90fb9257530d57e250
alex <xalexpintax@gmail.com>
2016-05-29 22:33:27 +0300

    added test for email

---
+++ src/test/java/service/EmailServiceImplTest.java
@@ -0,0 +1,27 @@
+package service;
+
+import configuration.SpringTestConfiguration;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import core.User;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringTestConfiguration.class)
+@TestPropertySource(locations="classpath:application.yml")
+public class EmailServiceImplTest {
+
+    @Autowired
+    EmailService emailService;
+
+    @Test
+    public void testSendConfirmEmail() throws Exception {
+        emailService.prepareService();
+        User user = new User("alex_pinta@ukr.net", "QAZxsw321");
+        emailService.sendConfirmEmail(user);
+    }
+}
