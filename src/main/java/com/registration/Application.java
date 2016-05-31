6c904c45c8cc4ec6c2bfb9dbb0707e301f1a7204
Oleg Pinta <oleg.sabfir@gmail.com>
2016-05-31 13:19:25 +0300

    single page attemption, no success

--- src/main/java/com/registration/Application.java
+++ src/main/java/com/registration/Application.java
@@ -3,10 +3,12 @@ package com.registration;
 
 import com.registration.controller.RegistrationController;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+@EnableAutoConfiguration
 @SpringBootApplication
 @Configuration
 @ComponentScan(basePackageClasses = RegistrationController.class)
--- src/main/java/com/registration/controller/RegistrationController.java
+++ src/main/java/com/registration/controller/RegistrationController.java
@@ -1,13 +1,40 @@
 package com.registration.controller;
 
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Map;
 
 @Controller
 public class RegistrationController {
-    @RequestMapping(value = "/")
-    public String index() {
-        return "index";
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String registrationMain() {
+        return "registration";
+    }
+
+    @RequestMapping(value = "/registration", method = RequestMethod.GET)
+    public ResponseEntity registrationPage() {
+        System.out.println("111");
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/registrationform", method = RequestMethod.GET)
+    public String registrationForm(Model model) {
+        System.out.println("Registrationform controller called");
+
+        /*if (model.containsAttribute("showfragment")) {
+            Map<String, Object> modelMap = model.asMap();
+            System.out.println("showfragment = " + modelMap.get("showfragment"));
+        }
+
+        model.addAttribute("showfragment", true);*/
+
+        return "fragments/fragment :: reg";
     }
 }
