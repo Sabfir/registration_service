7fb872cf694562e764e7cb99cbef3239085c8510
Oleg Pinta <oleg.sabfir@gmail.com>
2016-06-01 10:56:45 +0300

    rest api development

--- src/main/java/com/registration/controller/RegistrationController.java
+++ src/main/java/com/registration/controller/RegistrationController.java
@@ -1,13 +1,17 @@
 package com.registration.controller;
 
 
+import com.registration.model.RegistrationForm;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.validation.Valid;
 import java.util.Map;
 
 @Controller
@@ -17,24 +21,31 @@ public class RegistrationController {
         return "registration";
     }
 
-    @RequestMapping(value = "/registration", method = RequestMethod.GET)
-    public ResponseEntity registrationPage() {
-        System.out.println("111");
-
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
     @RequestMapping(value = "/registrationform", method = RequestMethod.GET)
     public String registrationForm(Model model) {
-        System.out.println("Registrationform controller called");
+        model.addAttribute("email", "enter email");
+        model.addAttribute("password", "enter password");
 
-        /*if (model.containsAttribute("showfragment")) {
-            Map<String, Object> modelMap = model.asMap();
-            System.out.println("showfragment = " + modelMap.get("showfragment"));
-        }
+        return "fragments/fragment :: registration";
+    }
 
-        model.addAttribute("showfragment", true);*/
+    @RequestMapping(value = "/velidation", method = RequestMethod.GET)
+    public String validate(@Valid RegistrationForm registrationForm, Model model) {
+        /*if (bindingResult.hasErrors()) {
+            return "fragments/fragment :: registration";
+        }*/
 
-        return "fragments/fragment :: reg";
+
+//        ResponseEntity res = new ResponseEntity(model, HttpStatus.OK);
+
+        model.addAttribute("email", registrationForm.getEmail());
+        model.addAttribute("password", registrationForm.getPassword());
+
+        return "fragments/success :: success";
+    }
+
+    @RequestMapping(value = "/success", method = RequestMethod.GET)
+    public String success() {
+        return "fragments/success :: success";
     }
 }
