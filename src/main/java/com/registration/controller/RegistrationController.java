5f451ea7f7cc7432b2609fd310c09282a5ac1b69
Oleg Pinta <oleg.sabfir@gmail.com>
2016-06-05 01:41:14 +0300

    error outputting

--- src/main/java/com/registration/controller/RegistrationController.java
+++ src/main/java/com/registration/controller/RegistrationController.java
@@ -5,13 +5,19 @@ import com.registration.util.TemplateBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 import com.registration.service.JmsService;
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.util.List;
 
 @Controller
+/*@SessionAttributes("user")*/
 public class RegistrationController {
     @Autowired
     private JmsService jmsService;
@@ -20,29 +26,72 @@ public class RegistrationController {
     private TemplateEngine templateEngine;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String registrationMain() {
-          return "registration";
+    public String registrationMain(User user) {
+       // model.addAttribute("showRegistration", true);
+        return "index";
     }
 
     @RequestMapping(value = "/registrationForm", method = RequestMethod.GET)
-    public String registrationForm(Model model) {
-        return "fragments/fragment :: registration";
+    public String registrationForm(@ModelAttribute("user") User user, Model model) {
+        //model.addAttribute("user", new User("email@gmail.com", "password123"));
+        //model.addAttribute("showRegistration", true);
+        //User user = new User("", "");
+        //model.addAttribute("user", user);
+
+        return "fragments/registration";
     }
+
 
     @RequestMapping(value = "/validation", method = RequestMethod.POST)
-    @ResponseBody
-    public String validateCredentials(HttpServletRequest request) {
-        jmsService.sendMessage(new User(request.getParameter("email"), request.getParameter("password")));
-        return TemplateBuilder.processTemplate("fragments/success", new Context());
+    public String validateCredentials(@Valid User user, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "fragments/registration";
+        }
+
+        model.addAttribute("email", user.getEmail());
+        model.addAttribute("password", user.getPassword());
+        return "fragments/success";
     }
 
-    @RequestMapping(value = "/success", method = RequestMethod.GET)
-    public String success() {
-        return "fragments/success :: success";
+    @RequestMapping(value = "/validation1", method = RequestMethod.POST)
+    @ResponseBody
+    //public String validateCredentials(@Valid ModelAttribute("user") User user, BindingResult bindingResult) {
+    public String validateCredentials(@Valid @ModelAttribute("user") User user, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            // TODO logging
+            System.out.println("Some errors occurred while validate user");
+
+            String errors = "";
+            for (Object object : bindingResult.getAllErrors()) {
+                if(object instanceof FieldError) {
+                    FieldError fieldError = (FieldError) object;
+
+                    errors += fieldError.getField();
+                }
+
+                if(object instanceof ObjectError) {
+                    ObjectError objectError = (ObjectError) object;
+
+                    errors += objectError.toString();
+                }
+            }
+
+            Context context = new Context();
+            context.setVariable("passworderror", "some error");
+            context.setVariable("user", user);
+            context.setVariable("errors", errors);
+            return TemplateBuilder.processTemplate("fragments/registration", context);
+        } else {
+            // TODO logging
+            System.out.println("User validated successfully");
+
+            jmsService.sendMessage(user);
+            return TemplateBuilder.processTemplate("fragments/success", new Context());
+        }
     }
 
     @RequestMapping(value = "/welcomePage/{hashCodeMessage}", method = RequestMethod.GET)
     public String welcomePage(@PathVariable String hashCodeMessage) {
-        return "fragments/fragment :: welcomepage";
+        return "fragments/welcome";
     }
 }
